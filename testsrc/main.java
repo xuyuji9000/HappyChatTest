81ed3b935f2026123225d32279ec5582f8321e6e
xuyuji9000 <xuyuji9000@163.com>
2014-04-02 21:35:55 +0800

    use junit to test ServerFrame
    it's still the beginning.

--- testsrc/main.java
+++ testsrc/main.java
@@ -3,9 +3,6 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		testServerFrame.showTheFrame();
-		testServerFrame.test();
-		testServerFrame.tearDown();
 		
 	}
 
--- testsrc/testServerFrame.java
+++ testsrc/testServerFrame.java
@@ -1,23 +1,5 @@
-import static org.junit.Assert.*;
-import org.fest.swing.fixture.FrameFixture;
-import org.junit.Test;
-
+import junit.framework.TestCase;
 
 public class testServerFrame {
-	private static FrameFixture frame;
-	@Test
-	public static void showTheFrame() {
-		frame = new FrameFixture(new ServerFrame());
-		frame.show();
-//		fail("Not yet implemented");
-	}
-	public static void tearDown()
-	{
-		frame.cleanUp();
-	}
-	public static void test()
-	{
-		frame.textBox("btn1").click();
-	}
-
+	
 }
